d80ae4975fc29dd0d153043fbc1e904d854defa7
ShadowSlayer03 <sslayer441@gmail.com>
2024-01-04 14:38:33 +0530

    Changed help.jsx and added db files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,8 @@ function HomePage() {
     <>
       <Help />
       <Navbar />
-      
+      < Hero />
+      <Footer />
     </>
   );
 }
--- client/src/components/Help.jsx
+++ client/src/components/Help.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 const Help = () => {
   return (
-    <div className='flex justify-center p-4 bg-blue-600'>In case of emergency, &nbsp;
-      <div className='underline font-semibold'>don't use this app</div>
-      <div className='text-white font-bold'>&nbsp;call us at</div><a href="tel:+9152987821"><div className='text-blue-500 hover:underline text-black'>&nbsp;9152987821</div></a>
+    <div className='flex flex-col items-center p-4 bg-blue-600 text-white'>
+      <p>In case of an emergency, please <span className='underline font-semibold'>refrain from using this website.</span> <span className='pl-1.5 font-bold'>Instead, reach us directly by calling </span><a href="tel:+9152987821" className='text-blue-500 hover:underline text-neutral-200'>+9152987821</a></p>
+      <p><span className='font-semibold'>Your safety and well-being are our top priorities!</span></p>
     </div>
-  )
-}
+  );
+};
 
-export default Help
+export default Help;
