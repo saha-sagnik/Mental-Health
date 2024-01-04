58b3237d960c1d73e7ebf5e1e1fe2f76880f5bb1
saha-sagnik <sagnik12347@gmail.com>
2024-01-04 10:28:31 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "../components/Navbar"
+import {BrowserRouter as Routes,Route} from 'react-router-dom'
+function App() {
+  return (
+    <Navbar/>
+    
+  )
+}
+
+export default App
