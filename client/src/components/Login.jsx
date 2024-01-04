bd2c1de2827e52f25b2c85639a94ea8df9a4d370
Ajay-S-Biradar <sbmunnu@gmail.com>
2024-01-04 14:19:16 +0530

    login states and backend

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import { NavLink, Navigate, useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import google from '../assets/google.png'
 import woman from '../assets/login-pic.jpg'
+import axios from "axios";
 import 'flowbite'
 
 
 const Login = () => {
+  const [mail,setMail] = useState(); 
+  const [pass,setPass] = useState(); 
+  const [showalert,setShowAlert] = useState(false);
+  const navigate = useNavigate()
+
+  async function submit(e){
+    e.preventDefault();
+    try{
+        await axios.post("http://localhost:5001/",{
+            mail,
+            pass
+        })
+        .then(res=>{
+            console.log(res);
+            if(res.data=='fail'){
+              setShowAlert(true);
+              setTimeout(() => {
+                setShowAlert(false);
+              }, 4000);
+            }
+            else navigate('/');
+        })
+    }
+    catch(e){
+        console.log(e);
+    }
+  }
+
   return (
+    <>
+    {
+      showalert?
+    <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+      <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+      </svg>
+      <span class="sr-only">Info</span>
+      <div>
+        <span class="font-medium">Login Failed!</span> Change input credentials and try submitting again.
+      </div>
+    </div>
+    :
+    ""
+    }
+
     <section className='bg-gray-50 dark:bg-gray-900 flex flex-wrap'>
       <div className='flex flex-col items-center justify-center px-8 py-8 mx-auto md:h-screen lg:py-0'>
         <div className='w-full bg-yellow-100 rounded-lg border border-gray-300 shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
@@ -23,11 +68,24 @@ const Login = () => {
               <div className='flex gap-4 flex-col-1'>
                 <div>
                   <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                  <input type="email" name="email" id="email" class="bg-gray-50  pr-10 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="yourmail@mail.com" required="" />
+                  <input type="email" name="email" id="email" 
+                  class="bg-gray-50  pr-10 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  placeholder="yourmail@mail.com" 
+                  required="" 
+                  onChange={(e)=>{
+                    setMail(e.target.value);
+                  }}
+                  />
                 </div>
                 <div>
                   <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                  <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border pr-10 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                  <input type="password" name="password" id="password" 
+                  placeholder="••••••••" 
+                  class="bg-gray-50 border pr-10 border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                  onChange={(e)=>{
+                    setPass(e.target.value);
+                  }}
+                  required="" />
                 </div>
               </div>
             </form>
@@ -54,7 +112,10 @@ const Login = () => {
               </div>
               <a href="#" class="text-sm font-medium text-red-600 hover:underline dark:text-blue-500">Forgot password?</a>
             </div>
-            <button type="submit" class="w-full text-white bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+            <button type="submit" 
+            class="w-full text-white bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  onClick={submit}
+            >Sign in</button>
                   <p class="text-sm font-light text-gray-500 dark:text-gray-400"></p>
           </div>
         </div>
@@ -63,6 +124,7 @@ const Login = () => {
         <img src={woman} alt="" />
       </div>
     </section>
+    </>
   )
 }
 
--- server/app.js
+++ server/app.js
@@ -1,9 +1,95 @@
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const cors = require("cors");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors());
+
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+      user:"ajaysbiradar3@gmail.com",
+      pass: "lqhvdmghcavraxop"
+    }
+})
+
+async function connectToMongoDB() {
+    try {
+      await mongoose.connect('mongodb+srv://sbmunnu:munnu@mongo.mzaad3h.mongodb.net/wallmart?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log('Connected to MongoDB');
+    } catch (error) {
+      console.error('Error connecting to MongoDB:', error);
+    }
+  }
+connectToMongoDB()
+Schema = mongoose.Schema 
+const userSchema = new Schema({
+    name:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    email:{
+        type:String
+    }
+})
+const User = mongoose.model('User', userSchema)
+
+app.get('/',cors(),(req,res)=>{
+})
+
+app.post('/',async(req,res)=>{
+    const {mail,pass} = req.body;
+    console.log(mail);
+
+    User.findOne({email:mail}).then((users)=>{
+        if (users) {
+          // User exists in the database
+          console.log('User found:', users);
+          // Perform actions for existing user 
+          if(users.password==pass){
+            console.log("Successful in login")
+            res.json("success");
+          }
+          else{
+            console.log('password and username not matched')
+            res.json("fail");
+          }
+        } else {
+          console.log('User not found');
+          res.json("fail");
+        }
+    
+      })
+      .catch((err)=>{
+        res.json("fail");
+      })
+})
+
+app.post('/signup',async (req,res)=>{
+    otp = Math.floor(100000 + Math.random() * 900000);
+    const {mail} = req.body;
+    const mailOptions = {
+      from: 'ajaysbiradar3@gmail.com',
+      to: mail,
+      subject: 'OTP',
+      text: otp.toString()
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+  
+        } else {
+          res.json(otp);
+        }
+    });
+})
 
 app.listen(5001,()=>{
     console.log("running at 5001");
