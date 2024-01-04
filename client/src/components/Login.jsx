f41c2c0d01174cd5204b8943ba6c1f4e0f72fc40
saha-sagnik <sagnik12347@gmail.com>
2024-01-04 16:00:40 +0530

    UI

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -41,22 +41,22 @@ const Login = () => {
     <>
       {
         showalert ?
-          <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+          <div class="flex items-center p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400" role="alert">
             <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
               <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
             </svg>
             <span class="sr-only">Info</span>
             <div>
-              <span class="font-medium">Login Failed!</span> Change input credentials and try submitting again.
+              <span class="font-medium">Login Failed!</span> Change input cblueentials and try submitting again.
             </div>
           </div>
           :
           ""
       }
 
-      <section className='bg-gray-50 dark:bg-gray-900 flex flex-wrap'>
+      <section className='bg-blue-50 dark:bg-gray-900 flex flex-wrap'>
         <div className='flex flex-col items-center justify-center px-8 py-8 mx-auto md:h-screen lg:py-0'>
-          <div className='w-full bg-yellow-100 rounded-lg border border-gray-300 shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+          <div className='w-full bg-blue-100 rounded-lg border border-gray-300 shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
             <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
               <div className='flex justify-center items-center'>
                 <img className='h-10 m-3' src={logo} alt="" />
@@ -67,7 +67,7 @@ const Login = () => {
               <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white '>
                 Welcome Back
               </h1>
-              <p className='text-xs font-light text-gray-500 dark:text-gray-400'>Find your pet friend in seconds. Don't have an account? <NavLink to='/signup' className='font-medium text-orange-600 hover:underline dark:text-orange-500'>Sign Up.</NavLink> </p>
+              <p className='text-xs font-light text-gray-500 dark:text-gray-400'>Unlock your inner peace. Don't have an account? <NavLink to='/signup' className='font-medium text-blue-600 hover:underline dark:text-blue-500'>Sign Up.</NavLink> </p>
               <form className='space-y-4 md:space-y-6' action="">
                 <div className='flex gap-4 flex-col-1'>
                   <div>
@@ -100,9 +100,9 @@ const Login = () => {
                   or
                 </p>
               </div>
-              <div className='w-full flex items-center justify-center gap-0 bg-white rounded-lg shadow hover:bg-orange-300 cursor-pointer transition duration-300 ease-in-out dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
+              <div className='w-full flex items-center justify-center gap-0 bg-white rounded-lg shadow hover:bg-blue-300 cursor-pointer transition duration-300 ease-in-out dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
                 <img className='pl-10 w-16' src={google} />
-                <h1 className='dark:text-gray-400 p-3 text-sm hover:text-white hover:bg-orange-300 cursor-pointer transition duration-300 ease-in-out'>Sign in with Google</h1>
+                <h1 className='dark:text-gray-400 p-3 text-sm hover:text-white hover:bg-blue-300 cursor-pointer transition duration-300 ease-in-out'>Sign in with Google</h1>
               </div>
 
               <div class="flex items-center justify-between">
@@ -114,10 +114,10 @@ const Login = () => {
                     <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                   </div>
                 </div>
-                <a href="#" class="text-sm font-medium text-red-600 hover:underline dark:text-blue-500">Forgot password?</a>
+                <a href="#" class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Forgot password?</a>
               </div>
               <button type="submit"
-                class="w-full text-white bg-yellow-600 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="w-full text-white bg-blue-600 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 onClick={submit}
               >Sign in</button>
               <p class="text-sm font-light text-gray-500 dark:text-gray-400"></p>
