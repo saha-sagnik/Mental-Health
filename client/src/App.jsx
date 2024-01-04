46a35127962c1f54116261c10a86e0927b0b51bb
saha-sagnik <sagnik12347@gmail.com>
2024-01-04 11:50:26 +0530

    routers added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,29 @@
-import React from "react"
-import Navbar from "../components/Navbar"
-import {BrowserRouter as Routes,Route} from 'react-router-dom'
-import Help from "../components/Help"
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Login from '../components/Login';
+import Help from "../components/Help";
+
 function App() {
   return (
-    <>
-    <Help/>
-    <Navbar/>
-    </>
-    
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+
+function HomePage() {
+  return (
+    <>
+      <Help />
+      <Navbar />
+      
+    </>
+  );
+}
+
+export default App;
