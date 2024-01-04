f7292fca024a4d6143ad42d09bea3fbe81eb285a
saha-sagnik <sagnik12347@gmail.com>
2024-01-04 12:35:39 +0530

    login page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Login from '../components/Login';
-import Help from "../components/Help";
+import Navbar from "./components/Navbar";
+import Login from './components/Login';
+import Help from "./components/Help";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -10,18 +11,17 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
 }
-
 
 function HomePage() {
   return (
     <>
       <Help />
       <Navbar />
-      
     </>
   );
 }
