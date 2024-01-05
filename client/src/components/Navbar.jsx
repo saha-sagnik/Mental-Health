dbb516693b1e6a787385882c25ec53c567415717
ShadowSlayer03 <sslayer441@gmail.com>
2024-01-05 12:29:22 +0530

    Added some files realated to passport.js

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,6 +7,9 @@ import { useSelector } from 'react-redux'
 const Navbar = () => {
   const navigate = useNavigate()
   const loggedin = useSelector(Store=>Store.info.loggedIn) ;
+  if(!loggedin){
+    navigate('/login');
+  }
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
