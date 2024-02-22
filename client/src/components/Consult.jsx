7d88153a86e8d0213121383c35828ab74211ffb7
ShadowSlayer03 <sslayer441@gmail.com>
2024-02-22 14:13:42 +0530

    Resolved image url issue in home. Beautified consult page

--- client/src/components/Consult.jsx
+++ client/src/components/Consult.jsx
@@ -24,32 +24,36 @@ const generateImages = (count) => {
 const Consult = () => {
     return (
         <>
-        <section className='bg-[#f8e9e6]'>
+        <section className='bg-[#f8e9e6] mb-12'>
             <div className='grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12'>
                 <div className="mr-auto place-self-center lg:col-span-7">
                     <h1 className="max-w-xl mb-4 text-4xl font-semibold tracking-tight leading-none md:text-5xl xl:text-6x">Take consultation from the best</h1>
 
                     <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">Feel free to reach out for consultation—our support is here for you. Your well-being matters to us.</p>
-                    <div className='flex'>
+                    <div className='flex items-center gap-3'>
                         {generateImages(3)}
                         <div className='px-2'>+69 Doctors are online</div>
                         <span class="relative flex mt-2 h-2 w-2">
                             <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                             <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                         </span>
-
                     </div>
-                    <Link href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-500">
+                    <Link href="#" class="inline-flex items-center justify-center mt-5 px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-blue-500">
                         Get started
                     </Link>
-                    <div className=" py-8 gap-4 flex items-center text-base">
-                        <div className='flex'><box-icon name='award'></box-icon>
-                    <p>Verified Doctors</p></div>
-                    <div className='flex'><box-icon name='report' type='solid' ></box-icon>
-                    <p>Digital prescription</p></div>
-                    <div className='flex'><box-icon name='message-square-detail' ></box-icon>
-                    <p>Free Followup</p></div>
-                    
+                    <div className=" py-8 gap-7 flex items-center text-base">
+                    <div className='flex gap-1'>
+                    <box-icon name='award'></box-icon>
+                    <p>Verified Doctors</p>
+                    </div>
+                    <div className='flex gap-1'>
+                    <box-icon name='report' type='solid' ></box-icon>
+                    <p>Digital prescription</p>
+                    </div>
+                    <div className='flex gap-1'>
+                    <box-icon name='message-square-detail' ></box-icon>
+                    <p>Free Followup</p>
+                    </div>
                     </div>
                 </div>
                 <div className="hidden w-[600px] h-[300px] lg:flex">
@@ -58,16 +62,16 @@ const Consult = () => {
             </div>
         </section>
         {/* Our Doctors section */}
-        <div className='p-4 text-3xl'>
-            <h1 className='font-semibold text-md'>Our Doctors</h1>
-            <div className='grid grid-cols-3 h-full'>
+        <div className='p-4 text-3xl flex flex-col items-center'>
+            <h1 className='font-semibold underline text-4xl mb-10 ml-24'>Our Doctors</h1>
+            <div className='grid grid-cols-3 h-full w-[90%]'>
             {doctorItems.map((doctor, index) => (
                 <DoctorCard
                     key={index}
                     name={doctor.name}
                     position={doctor.position}
                     imageUrl={doctor.img}
-                    specialized={[doctor.specialised]}
+                    specialized={[doctor.specialized]}
                 />
             ))}
         </div>
--- client/src/components/DoctorCard.jsx
+++ client/src/components/DoctorCard.jsx
@@ -3,22 +3,27 @@ import React from 'react';
 
 const DoctorCard = ({ name, position, imageUrl, specialized }) => {
     return (
-        <div className="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white shadow-lg bg-clip-border rounded-xl h-80">
-            <img src={imageUrl} alt={name} />
-            <div className="p-6 text-center">
-                <h4 className="block mb-2 font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
+        <div className="relative mx-4 mt-4 pb-3 overflow-hidden text-gray-700 bg-white shadow-xl bg-clip-border rounded-xl h-[92%]">
+            <img src={imageUrl} alt={name} className='w-full h-[78%] object-cover object-top'/>
+            <div className="p-5 text-center flex flex-col gap-1">
+                <h4 className="block font-sans text-2xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
                     {name}
                 </h4>
-                <p className="block font-sans text-base antialiased font-medium leading-relaxed text-transparent bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
+                <h5 className="block font-sans text-base antialiased font-medium leading-relaxed bg-clip-text bg-gradient-to-tr from-blue-gray-600 to-blue-gray-400">
                     {position}
-                </p>
+                </h5>
             </div>
-            <div className="flex justify-center p-6 pt-2 gap-7">
+            <div className="flex justify-center ">
                 {/* Assuming specialized is an array, you might want to map through it */}
                 {specialized.map((specialty, index) => (
-                    <span key={index} className="text-sm text-blue-gray-700">
-                        {specialty}
+                    <div className='flex gap-2'>
+                    <span className="text-sm text-blue-gray-700">
+                      Expertise: 
                     </span>
+                    <span key={index} className="text-sm text-blue-gray-700">
+                      {specialty}
+                    </span>
+                    </div>
                 ))}
             </div>
         </div>
